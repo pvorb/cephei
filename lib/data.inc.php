c5ba42fec918c19d86ed7c3998639ca67008f74a
Paul Vorbach <p.vorbach@gmail.com>
2010-04-05 00:23:58 +0200

    Confiruration loading enabled.

--- lib/data.inc.php
+++ lib/data.inc.php
@@ -1,17 +1,22 @@
 <?php
 // TODO Insert functions for redirecting and 404 error.
 
+// Load configuration
+foreach ($db->query('SELECT name, value FROM conf', PDO::FETCH_NUM) as $row) {
+	$conf[$row[0]] = $row[1];
+}
+
 // Try to find item with $path that was in $_GET['q'].
-$result = $db->query('SELECT * FROM content_item WHERE path = "'.$path.'"');
-$row = $result->fetch(PDO::FETCH_ASSOC);
+$res = $db->query('SELECT * FROM content_item WHERE path = "'.$path.'"');
+$row = $res->fetch(PDO::FETCH_ASSOC);
 if ($row == false) {
 	// Try to find out, if there have been older versions of this item at $path.
-	$result = $db->query('SELECT * FROM path_redirect WHERE source_path = "'.$path.'"');
-	$row = $result->fetch(PDO::FETCH_ASSOC);
+	$res = $db->query('SELECT * FROM path_redirect WHERE source_path = "'.$path.'"');
+	$row = $res->fetch(PDO::FETCH_ASSOC);
 	if ($row != false) {
 		// Get the equivalent item.
-		$result = $db->query('SELECT path FROM content_item WHERE id = "'.$row['target_id'].'"');
-		$row = $result->fetch(PDO::FETCH_ASSOC);
+		$res = $db->query('SELECT path FROM content_item WHERE id = "'.$row['target_id'].'"');
+		$row = $res->fetch(PDO::FETCH_ASSOC);
 		if ($row != false) {
 			// If it exists, redirect to the updated URL.
 			header('HTTP/1.1 301 Moved Permanently');
