abf7d2856a23b9495469ad349f904a9a4152d93a
Paul Vorbach <p.vorbach@gmail.com>
2010-04-29 20:42:30 +0200

    Removed deprecated content of lib/admin.inc.php.

--- lib/admin.inc.php
+++ lib/admin.inc.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * This file manages the administration.
+ * This file manages the 
  *
  * @author Paul Vorbach <p.vorbach@genitis.org>
  * @license http://opensource.org/licenses/mit-license.php MIT License
