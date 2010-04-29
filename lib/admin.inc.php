ec5572d07ff26a8d5c432a3c8a21a8342a394bff
Paul Vorbach <p.vorbach@gmail.com>
2010-04-29 20:55:29 +0200

    Comment change

--- lib/admin.inc.php
+++ lib/admin.inc.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * This file manages the 
+ * This file manages the administration area.
  *
  * @author Paul Vorbach <p.vorbach@genitis.org>
  * @license http://opensource.org/licenses/mit-license.php MIT License
