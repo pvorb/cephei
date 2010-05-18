c5163b7919fb3540ee593bc5944435e06b8531f1
Paul Vorbach <p.vorbach@gmail.com>
2010-05-18 02:33:00 +0200

    Started writing test cases.

--- lib/test/auth.test.php
+++ lib/test/auth.test.php
@@ -1,9 +1,20 @@
 <?php
 /**
- * DESCRIPTION
+ * Tests the Authentification module.
  *
  * @author Paul Vorbach <p.vorbach@gmail.com>
  * @license http://opensource.org/licenses/mit-license.php MIT License
- * @package 
+ * @package org.genitis.cms.tests
  */
+
+require_once 'PHPUnit/Extensions/Database/Testcase.php';
+require_once '../mod/auth.mod.php';
+/**
+ * @todo Implement data tests.
+ */
+class auth_test extends PHPUnit_Extensions_Database_TestCase {
+	public function test_authorization() {
+		
+	}
+}
 ?>
