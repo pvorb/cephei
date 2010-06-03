ef363257075bd9fe01ee241657880e87235e3b3e
Paul Vorbach <p.vorbach@gmail.com>
2010-06-03 23:10:56 +0200

    Added some new modules whose features aren't implemented yet.

---
+++ lib/mod/wiki.mod.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Wiki module
+ *
+ * @author Paul Vorbach <p.vorbach@gmail.com>
+ * @license http://opensource.org/licenses/mit-license.php MIT License
+ * @package org.genitis.cms.mod.wiki
+ */
+
+require_once 'mod.def.php';
+
+class wiki extends mod {
+	static $name = 'Wiki Module';
+	static $author = 'Paul Vorbach';
+	static $email = 'p.vorbach@gmail.com';
+	static $version = '0.1.0';
+	static $type = 'extra';
+}
+?>
